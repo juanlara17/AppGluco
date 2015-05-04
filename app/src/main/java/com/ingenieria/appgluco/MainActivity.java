7f73d47e06150b98649b773fed292e07f7edeeb7
Juan Felipe Lara Martinez <juanlara17@hotmail.com>
2015-05-04 00:49:10 -0500

    Commit

--- app/src/main/java/com/ingenieria/appgluco/MainActivity.java
+++ app/src/main/java/com/ingenieria/appgluco/MainActivity.java
@@ -2,10 +2,12 @@ package com.ingenieria.appgluco;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.SyncAdapterType;
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -18,8 +20,11 @@ import android.widget.Toast;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,18 +39,20 @@ import java.util.ArrayList;
 public class MainActivity extends ActionBarActivity {
 
     EditText user, pass;
-    Button  Login;
+    Button  login;
     TextView   registrar;
     Httppostaux    post;
     String userFTP = "juanlara@taxy.co";
     String passFTP = "6P9#DTH-$+hK";
     String IP_Server = "ftp.taxy.co";
-    String URL_connect = "ftp://ftp.taxy.co/acces.php";
-    boolean result_back;
+    String URL_connect = "ftp://juanlara@taxy.co/acces.php";
+    boolean mConnect;
     private ProgressDialog pDialog;
     public FTPClient mFTPCLiente;
-    public HttpClient httpClient;
 
+    JSONParser jsonParser = new JSONParser();
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,12 +62,12 @@ public class MainActivity extends ActionBarActivity {
 
         user = (EditText)findViewById(R.id.etUser);
         pass = (EditText)findViewById(R.id.etPassword);
-        Login = (Button)findViewById(R.id.btLogin);
+        login = (Button)findViewById(R.id.btLogin);
         registrar = (TextView)findViewById(R.id.tvRegistro);
 
 
         /********************Button login******************/
-        Login.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String usuario = user.getText().toString();
@@ -90,42 +97,38 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public boolean loginstatus (String username, String password){
-        int logstatus = -1;
+        int success;
 
         ArrayList<NameValuePair>    postparameters2send = new ArrayList<NameValuePair>();
-                                    postparameters2send.add(new BasicNameValuePair("usuario", username));
+                                    postparameters2send.add(new BasicNameValuePair("username", username));
                                     postparameters2send.add(new BasicNameValuePair("password", password));
 
-        String response = null;
-
         try {
-            response = CustomHttpClient.executeHttpPost(URL_connect, postparameters2send);
-            String res = response.toString();
-            res = res.replaceAll("\\s+","");
+            JSONObject json = jsonParser.makeHttpRequest("http://appgluco.esy.es/appgluco/login.php", "POST",
+                    postparameters2send);
+            SystemClock.sleep(950);
+            success = json.getInt(TAG_SUCCESS);
 
-            if (res.equals("1")){
-                Log.e("Registro: ", "Valido");
+            if(success == 1){
+                JSONObject json_data;
+
+                Log.d("Login Successful!", json.toString());
+                // save user data
+                SharedPreferences sp = PreferenceManager
+                        .getDefaultSharedPreferences(MainActivity.this);
                 return true;
-            }else {
-                Log.e("Registro: ", "Invalido");
+            }else{
+                Log.e("JSON", "ERROR");
                 return false;
             }
-        } catch (Exception e) {
+        } catch (JSONException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    private boolean checklogindata(String usuario, String contraseña) {
-        if (usuario.equals("") || contraseña.equals("")){
-            Log.e("Login UI", "checklogindata user or pass error");
-            return false;
-        }else{
-            return true;
-        }
-    }
-
     class asynclogin extends AsyncTask<String, String, String>{
+
 
         String user, pass;
 
@@ -141,18 +144,6 @@ public class MainActivity extends ActionBarActivity {
         protected String doInBackground(String... strings) {
             user = strings[0];
             pass = strings[1];
-
-            try {
-                FTPClient ftpClient = new FTPClient();
-                ftpClient.connect(InetAddress.getByName(IP_Server));
-                ftpClient.login(userFTP, passFTP);
-                ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-                ftpClient.enterLocalPassiveMode();
-                Toast.makeText(getApplication(), "INGRESO CON EXITO A LA BASE DE DATOS",Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                Toast.makeText(getApplication(), "FALLA EN EL INGRESO",Toast.LENGTH_SHORT).show();
-                e.printStackTrace();
-            }
 
             if(loginstatus(user,pass) == true){
                 return "ok";
@@ -170,8 +161,22 @@ public class MainActivity extends ActionBarActivity {
                 Intent i = new Intent(MainActivity.this, Patient.class);
                 i.putExtra("user", user);
                 startActivity(i);
+            }else{
+                Vibrator vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+                vibrator.vibrate(200);
+                Toast.makeText(getApplication(), "Error: Cedula y/o Contraseña Incorrectos", Toast.LENGTH_LONG).show();
             }
+
         }
     }
+
+    private boolean checklogindata(String usuario, String contraseña) {
+        if (usuario.equals("") || contraseña.equals("")) {
+            Log.e("Login UI", "checklogindata user or pass error");
+            return false;
+        }else{
+            return true;        }
+    }
+
 
 }
